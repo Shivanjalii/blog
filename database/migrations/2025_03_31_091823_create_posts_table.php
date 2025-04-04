699149b57ca951ba9bc7723b5bb291e739ba61d3
Shivanjalii <shivanjali1545@gmail.com>
2025-04-04 11:23:17 +0530

    tag migration

--- database/migrations/2025_03_31_091823_create_posts_table.php
+++ database/migrations/2025_03_31_091823_create_posts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug')->unique(); 
             $table->text('content');
             $table->bigInteger('user_id');
             $table->timestamps();
