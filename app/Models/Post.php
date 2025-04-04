63ae09ccaea15de88043a13d96cbe14ff24657e6
Shivanjalii <shivanjali1545@gmail.com>
2025-04-04 19:14:38 +0530

    create post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,28 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'title',
-        'content',
-        'post_type',
-        'meta_data',
-        'category_id',
-        'author_id',
+        'title',       
+        'content',     
+        'post_type',   
+        'meta_data',   
+        'category_id', 
+        'user_id',     
     ];
 
-    public function category(){
+    protected $casts = [
+        'meta_data' => 'array', 
+    ];
+
+    /**
+     * Each post belongs to one category.
+     */
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function tags(){
-        return $this->belongsToMany(Tag::class);
+    /**
+     * A post can belong to many tags.
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag', 'post_id', 'tag_id');
     }
 
+    /**
+     * Each post belongs to a user (author).
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
-
