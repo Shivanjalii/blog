41710b4fc43c7bc14dd070a0f6f3f81cc4cfe10b
Shivanjalii <shivanjali1545@gmail.com>
2025-04-04 11:36:14 +0530

    categories_post migration

--- database/migrations/2025_04_04_060506_create_categories_post_table.php
+++ database/migrations/2025_04_04_060506_create_categories_post_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_tag', function (Blueprint $table) {
+        Schema::create('categories_post', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('categories_post');
     }
 };
