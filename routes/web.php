508b2d883d55c8f15f333ef2cd19275760775ac0
Shivanjalii <shivanjali1545@gmail.com>
2025-05-05 10:48:31 +0530

    create categoty and post and tag routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+ 
+Route::resource('categories', CategoryController::class);
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::resource('tags', TagController::class);
+
